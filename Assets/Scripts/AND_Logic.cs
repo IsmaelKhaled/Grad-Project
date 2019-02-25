891ee1a55aadb53d8c47557603927d682db30836
IsmaelKhaled <31388232+IsmaelKhaled@users.noreply.github.com>
2019-02-25 17:40:45 +0200

    Added logic lab scene
    Added logic lab scene

---
+++ Assets/Scripts/AND_Logic.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AND_Logic : MonoBehaviour {
+
+	public int topInput;
+	public int botInput;
+	public int output;
+	public bool isActive;
+	// Use this for initialization
+	void Start () {
+		isActive = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(isActive){
+			if(topInput == 1 && botInput == 1)
+			{	
+				output = 1;
+			}
+			else
+			{
+				output = 0;
+			}
+		
+		}
+	}
+}
---
+++ Assets/Scripts/ConnectionControl.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConnectionControl : MonoBehaviour {
+
+	LineRenderer lineRenderer;
+	public GameObject Line;
+	// Use this for initialization
+	void Start () {
+		//  GameObject AND = transform.Find("AND").gameObject;
+		// GameObject AND1 = transform.Find("AND (1)").gameObject;
+    	// Transform start = AND1.transform.Find("Output Location");
+		// Transform end = AND.transform.Find("Top Input Location");
+		
+		// DrawLine(start.gameObject,end.gameObject,Color.green);
+	
+	        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void DrawLine(GameObject startObject, GameObject endObject, Color color)
+	{
+		Vector2 startPosition = startObject.transform.position;
+		Vector2 endPosition = endObject.transform.position;
+		Vector2 offset = (endPosition - startPosition) / 2.0f;
+		Vector2 position = startPosition + offset;
+		
+		Transform line = Instantiate<GameObject>(Line, Vector3.zero, Quaternion.identity).transform;
+		line.GetComponent<Renderer>().material.color = color;
+
+		line.position = position;
+		line.LookAt(startPosition);
+
+		Vector3 currentScale = line.localScale;
+		currentScale.z = (endPosition - startPosition).magnitude;
+		line.localScale = currentScale;
+	}
+
+}
