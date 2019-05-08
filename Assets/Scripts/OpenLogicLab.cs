fdd1f04b0fe36a20d47d9583cf7c45548fde9964
Mohamed abd elhady elsaadany <33707473+hadojin@users.noreply.github.com>
2019-05-08 17:05:49 +0200

    Mesh adjustment, Added load to labs and adjusted lightings

---
+++ Assets/Scripts/OpenLogicLab.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OpenLogicLab : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        SceneManager.LoadScene("Logic Gates 0");
+    }
+}
