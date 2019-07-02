706feb1358e0f1f7aed3b87ac098c59249511c2c
magedmabrouk <magedmabrouk@ymail.com>
2019-07-03 02:14:24 +0300

    binary numbers game

---
+++ Assets/binary numbers/Assets/scripts/GameManager.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject[] lambs;
+    public Text number;
+    float DecideTime = 0f;
+    bool Decided = false;
+    public GameObject panel;
+
+    public GameObject[] electricity;
+    public GameObject illumination;
+    [SerializeField]
+    bool illuminate = false;
+    float alpha = 0f;
+
+    [SerializeField]
+    int CurrentNumber;
+    [SerializeField]
+    int RandomNumber;
+    // Start is called before the first frame update
+    void Start()
+    {
+        electricity[0].SetActive(false); electricity[1].SetActive(false);
+        panel.SetActive(false);
+       // RandomNumber = Random.Range(0, 1024);
+        //number.text = "" + RandomNumber;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!Decided)
+        {
+            DecideTime += Time.deltaTime;
+            RandomNumber = Random.Range(0, 1024);
+            number.text = "" + RandomNumber;
+            if (DecideTime >= 1.5f) Decided = true;
+        }
+
+        if (illuminate) illuminating();
+
+    }
+
+    private void LateUpdate()
+    {
+        if (Decided)
+        {
+            CurrentNumber = 0;
+            for (int i = 0; i < lambs.Length; i++)
+            {
+                if (lambs[i].GetComponent<test>().Lighted)
+                {
+                    CurrentNumber += (int)Mathf.Pow(2f, (float)i);
+                }
+
+            }
+            if (CurrentNumber == RandomNumber)
+            {
+                number.text = "YaaaaY!";
+                Invoke("EndGame", 2f);
+                electricity[0].SetActive(true); electricity[1].SetActive(true);
+                illuminate = true;
+            }
+        }
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    void EndGame ()
+    {
+        panel.SetActive(true);
+    }
+
+    void illuminating()
+    {
+        alpha += 0.008f;
+        if (alpha <= 0.4353f)
+        {
+            illumination.GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, alpha);
+        }
+        else
+        {
+            illuminate = false;
+        }
+        
+
+    }
+}
---
+++ Assets/binary numbers/Assets/scripts/test.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class test : MonoBehaviour
+{
+    public bool Lighted = false;
+    [SerializeField] private Animator anim;
+    [SerializeField] float  allowTime;
+    bool allowed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (!allowed) allowTime += Time.deltaTime;
+
+        if (allowTime >= 0.6f)
+        {
+            allowed = true;
+            allowTime = 0f;
+
+        }
+
+        if (allowed)
+        {
+            if (Input.GetMouseButton(0))
+            {
+                allowed = false;
+                Vector3 clicked = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                Collider2D coll = GetComponent<Collider2D>();
+                if (coll.OverlapPoint(clicked))
+                {
+
+                    if (!Lighted)
+                    {
+                        anim.SetBool("hoba", true);
+                        anim.SetBool("TurnOn", true);
+                        Lighted = true;
+
+                    }
+                    else
+                    {
+                        anim.SetBool("hoba", false);
+                        anim.SetBool("TurnOn", false);
+                        Lighted = false;
+
+                    }
+
+
+
+                }
+
+            }
+        }
+        
+    }
+}
