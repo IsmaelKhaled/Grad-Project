993952dacadd9d91300c9b8d24ed8022463fc826
Menna Allah Muhammed Abd el rahman <menna.muhammed198@gmail.com>
2019-07-08 14:05:55 +0200

    Added Npc Spawn Feature

---
+++ Assets/Scripts/NPCSpawner.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCSpawner : MonoBehaviour
+{
+    public GameObject [] Npcs;
+    public GameObject[] SpawnPoints;
+
+    public static int SpawnedNpcCount = 0;
+    public int MaxSpawnedNpc = 3;
+    // Update is called once per frame
+    void Update()
+    {
+        if (SpawnedNpcCount < MaxSpawnedNpc)
+        {
+            int point = UnityEngine.Random.Range(0, SpawnPoints.Length);
+            int Npc = UnityEngine.Random.Range(0, Npcs.Length);
+            GameObject spawned = Instantiate(Npcs[Npc], SpawnPoints[point].transform);
+            spawned.AddComponent<NpcWalking>();
+            int destpoint = UnityEngine.Random.Range(0, SpawnPoints.Length); 
+            while(destpoint == point)
+            {
+                destpoint = UnityEngine.Random.Range(0, SpawnPoints.Length);
+
+            }
+            spawned.GetComponent<NpcWalking>().dest = SpawnPoints[destpoint];
+            spawned.GetComponent<NpcWalking>().source = SpawnPoints[point];
+            SpawnedNpcCount++;
+            
+        }
+    }
+}
---
+++ Assets/Scripts/NpcWalking.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NpcWalking : MonoBehaviour
+{
+    public GameObject source;
+    public GameObject dest;
+
+    NavMeshAgent agent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.stoppingDistance = 0.5f;
+        gameObject.SetActive(true);
+        //GetComponent<WalkingSound>().enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.SetDestination(dest.transform.position);
+        float dist = agent.remainingDistance;
+        Debug.Log(gameObject.name + " " + dist);
+        if (dist <= agent.stoppingDistance && !agent.pathPending)
+        {
+            NPCSpawner.SpawnedNpcCount--;
+            Destroy(gameObject);
+        }
+    }
+}
