5c4734d9d9bb9e95a3720de61d9277654cf7c986
Ahmed Gomaa <ahmed.gomaa5858@gmail.com>
2019-07-10 22:52:19 +0200

    Edited Main Menu

---
+++ Assets/MainMenu/CamRotation.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamRotation : MonoBehaviour
+{
+	public float speed=0.01f;
+	public Transform target;
+	void Update () 
+	{
+	    transform.LookAt(target);
+		transform.RotateAround(target.position, transform.up, Time.deltaTime * speed);
+	}
+
+    // Update is called once per frame
+}
